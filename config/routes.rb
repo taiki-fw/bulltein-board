b8f18a3c764245d3d26942f67a66adc98752633f
taiki-fw <taiki_52@outlook.jp>
2019-11-24 05:05:21 +0900

    add CRUD to board model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :boards
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to:'boards#index'
 end
