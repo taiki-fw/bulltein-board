471a2c80e08e6b608efb075346e503d89a43ed35
taiki-fw <taiki_52@outlook.jp>
2019-11-28 00:56:01 +0900

    add body-column to board-model and shwow detail in board show page

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -71,6 +71,6 @@ class BoardsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def board_params
-      params.require(:board).permit(:title)
+      params.require(:board).permit(:title,:body)
     end
 end
