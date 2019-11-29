590b23a0ed8853b2e7dfdc65200d613c7b022a8a
taiki-fw <taiki_52@outlook.jp>
2019-11-29 23:34:01 +0900

    boardのtestデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do |t|
+  Board.create(title: "board_#{t}", body: "this is number_#{t} board body")
+end
