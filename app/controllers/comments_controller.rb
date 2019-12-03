1f6fc3aa0dbb3f0cde341f5a058815d293483012
taiki-fw <taiki_52@outlook.jp>
2019-12-03 13:12:27 +0900

    commentの削除をajaxにしました

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,7 +13,7 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @comment.save
         @comments = @board.comments
-        format.js
+        format.js { flash[:notice] = 'Comment was successfully created.' }
       else
         format.html { render :new }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
@@ -25,8 +25,8 @@ class CommentsController < ApplicationController
     @board = Board.find(params[:board_id])
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to @board, notice: 'Comment was successfully destroyed.' }
-      format.json { head :no_content }
+      @comments = @board.comments
+      format.js { flash[:notice] = 'Comment was successfully destroy.' }
     end
   end
 
